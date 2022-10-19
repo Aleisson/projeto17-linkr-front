72ff73d59dea5081a9296c44bcd35fa393577db3
Caio-Coldebella <c232621@dac.unicamp.br>
2022-10-19 13:08:09 -0300

    criacao de elementos da tela home

---
+++ src/elements/Trending.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export default function Trending(){
+    return (
+        <>
+        <CONTENT>
+            
+        </CONTENT>
+        </>
+    );
+}
+
+const CONTENT = styled.div`
+    width: 300px;
+    height: 400px;
+    background-color: #171717;
+`;

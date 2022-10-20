b80ea40f23e944a972f55acaab69023be6ba7e2e
Aleisson <caioaleisson@gmail.com>
2022-10-20 15:31:08 -0300

    docs(Like.js): add styled and make custom icon

--- src/elements/Likes/Like.js
+++ src/elements/Likes/Like.js
@@ -1,14 +1,28 @@
 import styled from "styled-components";
-
+import { BsFillHeartFill } from 'react-icons/bs';
 function Like() {
 
     return (
-        <>
-            <h1>LIKE</h1>
-        </>
+        <StyledDiv>
+            <h1><CustomBsFillHeartFill /></h1>
+        </StyledDiv>
     )
 
 
 }
+const StyledDiv = styled.div`
+
+    background-color: #FFFFFF;
+    margin: 12vw auto;
+    width: 200px;
+    height: 200px;
+    display:flex;
+    align-items:center;
+    justify-content: center;
+
+`
+const CustomBsFillHeartFill = styled(BsFillHeartFill)`
+
+`
 
 export { Like };

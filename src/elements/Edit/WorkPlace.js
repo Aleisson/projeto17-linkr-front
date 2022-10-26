745173833964042e1a6f83b4c60aa068a1c943f1
Aleisson <caioaleisson@gmail.com>
2022-10-26 00:36:43 -0300

    feat(Edit.js): make input onchange function

--- src/elements/Edit/WorkPlace.js
+++ src/elements/Edit/WorkPlace.js
@@ -1,14 +1,57 @@
+import { useRef, useEffect, useState } from 'react';
+import styled from "styled-components";
 function WorkPlace() {
-    //postId receve o id do post
-    // token recebe a token do usuário
-    return (
-        <>
-          
 
+
+    const inputRef = useRef();
+    const [text, setText] = useState("teste");
+    console.log(text)
+    useEffect(() => {
+
+        inputRef.current.focus();
+
+    }, [])
+
+    function editDescription(description) {
+
+        setText(description);
+        
+    }
+
+    return (
+
+        <>
+            <CustomDiv>
+
+                <h1>useRef</h1>
+                
+                    <input name='description' value={text} type='text' ref={inputRef} onChange={(e) => editDescription(e.target.value)}></input>
+                
+
+
+
+            </CustomDiv>
         </>
     );
 
 
 }
+
+const CustomDiv = styled.div`
+
+    background-color:grey;
+    height: 200px;
+    width:50%;
+    margin:auto;
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    justify-content:space-around;
+    padding:20px;
+    
+
+
+`
+
 
 export { WorkPlace };
